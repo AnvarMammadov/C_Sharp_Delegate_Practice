e9a3a77c499577f9d9f42310bec03b11b9a3dde7
name <anvarmammadovv@gmail.com>
2023-09-23 21:04:09 +0400

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace Delegate_Practice
+{
+    public delegate void Func(string? str);
+    public class MyClass
+    {
+        public string? Str { get; set; }
+        public MyClass(string? str)
+        {
+            Str = str;
+        }
+
+        public void Space(string? str)
+        {
+            string? temp = "";
+            if (str != null)
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    temp += str[i];
+                    if (str.Length - 1 != i) temp += "_";
+                }
+            }
+            Str = temp;
+
+            Console.WriteLine($"Space : {Str}");
+        }
+
+        public void Reverse(string? str)
+        {
+            string? temp = "";
+            if(str != null)
+            {
+                for (int i=str.Length-1; i >=0; i--)
+                {
+                    temp+=str[i];
+                }
+            }
+            Str = temp;
+            Console.WriteLine($"Reverse : {Str}");
+        }
+
+
+    }
+
+
+    
+    public class Run
+    {
+        public void runFunc( Func temp, string? str) {
+            temp.Invoke(str);   
+        }
+
+    }
+    public class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            MyClass cls = new MyClass("Anvar");
+            Func funcDell = new Func(cls.Space);
+            funcDell += cls.Reverse;
+            Run run=new Run();
+            run.runFunc(funcDell, "Anvar");
+        }
+
+    }
+}
